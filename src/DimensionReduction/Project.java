a23ddeef333921e984388583f55da43c645d78a3
cristian <cristian.colliander@umu.se>
2021-03-09 17:13:27 +0100

    starting on project

---
+++ src/DimensionReduction/Project.java
@@ -0,0 +1,19 @@
+package DimensionReduction;
+
+import jsat.linear.DenseVector;
+import jsat.linear.SparseVector;
+
+import java.util.List;
+
+public class Project {
+
+
+
+    public static DenseVector project(SparseVector input, List<DenseVector> axis) {
+
+
+        return null;
+
+    }
+
+}
