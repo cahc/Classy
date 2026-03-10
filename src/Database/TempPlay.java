f946bf8200ced3857a455125a3eeaebbd7f5dc3c
crco0001 <crco3536>
2026-03-10 13:25:47 +0100

    sync

--- src/Database/TempPlay.java
+++ src/Database/TempPlay.java
@@ -11,6 +11,8 @@ public class TempPlay {
 
     public static void main(String[] arg) throws IOException {
 
+        //test
+
         Integer yesr = 2020;
 
         if(yesr.compareTo(2015) < 0 || yesr.compareTo(2020) > 0 ) System.out.println("ignore!");
